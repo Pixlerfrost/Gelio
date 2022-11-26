03f5acc49f162795649ee5e4dbe4c8c605b3ac66
Mohamad Youness <pixlerfrost@gmail.com>
2022-11-26 18:51:33 -0500

    .

--- Components/navbar.js
+++ Components/navbar.js
@@ -20,6 +20,7 @@ function Navbar() {
             icon={<HamburgerIcon />}
             ml="auto"
             mr={{ base: "2vw", md: 0 }}
+            background="#202023"
           />
         </Container>
       </Box>
