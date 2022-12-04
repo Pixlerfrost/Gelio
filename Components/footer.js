aff1b013334ab8efb48bbaa7d4ad88818a6f357b
Mohamad Youness <pixlerfrost@gmail.com>
2022-12-03 19:22:44 -0500

    .

--- Components/footer.js
+++ Components/footer.js
@@ -4,35 +4,33 @@ import Logo2 from "../Components/logo_2";
 function Footer() {
   return (
     <>
-      <Stack>
-        <Stack
-          direction="row"
-          background="#050505"
-          display="flex"
-          flexWrap="wrap"
-          justify="center"
-          p="6"
-          align="center"
-          pb={{ base: "5vh" }}
-        >
-          <Logo2 />
+      <Stack
+        direction="row"
+        background="#050505"
+        display="flex"
+        flexWrap="wrap"
+        justify="center"
+        p="6"
+        align="center"
+        pb={{ base: "5vh" }}
+      >
+        <Logo2 />
 
-          <Link href="privacy_policy" paddingX="1vw" color="white">
-            Privacy Policy
-          </Link>
-          <Link href="/" paddingX="1vw" color="white">
-            Twitter
-          </Link>
-          <Link href="/" paddingX="1vw" color="white">
-            Instagram
-          </Link>
-          <Link href="/" paddingX="1vw" color="white">
-            Github
-          </Link>
-          <Link href="/" paddingX="1vw" color="white">
-            LinkedIn
-          </Link>
-        </Stack>
+        <Link href="privacy_policy" paddingX="1vw" color="white">
+          Privacy Policy
+        </Link>
+        <Link href="/" paddingX="1vw" color="white">
+          Twitter
+        </Link>
+        <Link href="/" paddingX="1vw" color="white">
+          Instagram
+        </Link>
+        <Link href="/" paddingX="1vw" color="white">
+          Github
+        </Link>
+        <Link href="/" paddingX="1vw" color="white">
+          LinkedIn
+        </Link>
       </Stack>
     </>
   );
